92649b5ef3a3d8dc0bd27a714eb9fbeb78ca06dc
Priyesh47data <ruke9998@gmail.com>
2026-03-26 17:37:26 +0530

    Add SQL script to initialize DataWarehouse database

---
+++ scripts/init_database.sql
@@ -0,0 +1,22 @@
+ 
+ use master;
+ GO
+ CREATE DATABASE DataWarehouse;
+ GO
+ use DataWarehouse
+ GO
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
+
+
+
+
+
+
+
+ 
